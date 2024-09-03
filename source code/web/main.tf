ec4c4f0a98e867b7999d20925f260eb2153ca0ef
B2111933 <lamb2111933@student.ctu.edu.vn>
2024-09-04 01:15:58 +0700

    string

--- source code/web/main.tf
+++ source code/web/main.tf
@@ -4,7 +4,7 @@ resource "aws_instance" "server" {
   ami = "ami-066784287e358dad1"
   instance_type = "t2.micro"
   subnet_id = var.security_group
-  security_groups = var.subnet
+  security_groups = [var.subnet]
 
   tags = {
     Name = "myserver"
